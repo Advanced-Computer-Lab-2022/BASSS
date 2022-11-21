79d05d99e7fcddf205eaa465d24126aabde4dfa5
adham <adhamsaber912@gmail.com>
2022-11-21 23:27:21 +0200

    new frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 
 import Courses from './components/Courses';
 function App() {
