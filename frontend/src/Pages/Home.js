0d18311e9b674282250a3674a2bb0b911d2b3cd2
adham <adhamsaber912@gmail.com>
2022-11-28 20:15:59 +0200

    adham's work with full landing page

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,10 +1,14 @@
 import '../App.css'
+import Cards from '../components/Cards/Cards'
+import Footer from '../components/Footer/Footer'
 import Landing from '../components/Landing/Landing'
 
 function Home (){
     return(
         <>
         <Landing/>
+        <Cards/>
+        <Footer/>
         </>
     )
 }
