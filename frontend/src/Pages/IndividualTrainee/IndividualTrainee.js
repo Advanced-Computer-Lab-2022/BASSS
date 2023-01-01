4dfd02ba91c8ee13f83e5d3bee393ff241274f41
adham <adhamsaber912@gmail.com>
2023-01-02 01:10:25 +0200

    ADhoum

---
+++ frontend/src/Pages/IndividualTrainee/IndividualTrainee.js
@@ -0,0 +1,32 @@
+import '../../App.css'
+import { Link } from 'react-router-dom'
+import './IndividualTrainee.css'
+import Search from '../../components/Search/Search';
+import Navbar from '../../components/Navbar/Navbar';
+import IndividualTraineeNavBar from '../IndividualTrainee/IndividualTraineeNavBar/IndividualTraineeNavBar'
+import Courses from '../../components/Courses/Courses';
+import IndividualCourses from '../../components/Courses/IndividualCourses';
+import { useState } from 'react';
+import axios from 'axios';
+import Forgetpass from '../../components/Forgetpass/Forgetpass';
+import Downloadcert from '../../components/Downloadcert/Downloadcert';
+import Writenotes from '../../components/Writenotes/Writenotes';
+import Sendcertbymail from '../../components/Sendcertbymail/Sendcertbymail';
+
+function IndividualTrainee(){
+
+    return(
+        <>
+        <IndividualTraineeNavBar/>
+        <div className='IndividualTrainee-body'>
+
+        {/* <Writenotes/> */}
+
+        <IndividualCourses Link = "/individualtrainee/myCourseDetails"/>
+        <br></br><br></br><br></br><br></br><br></br><br></br>
+
+        </div>
+        </>
+    )
+}
+export default IndividualTrainee;
